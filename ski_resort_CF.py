4e53a545441c44a3a5f9847092e0e4c392e6d978
Sofia Zorich <133038243+sofiazorich@users.noreply.github.com>
2025-04-11 08:52:53 -0400

    Update ski_resort_CF.py

--- ski_resort_CF.py
+++ ski_resort_CF.py
@@ -8,9 +8,11 @@ import random
 # function to read in data
 def process_data(data_path):
     data = pd.read_csv(data_path)
+    # select only numeric columms
     feature_cols = ['elevation_difference_m', 'total_slope_length_km', 'number_of_lifts', 'annual_snowfall_cm']
     data = data[['name'] + feature_cols].set_index('name')
     scaler = StandardScaler()
+    # scale numeric columns
     data[feature_cols] = scaler.fit_transform(data[feature_cols])
     return data
 
@@ -19,30 +21,25 @@ def simulate_user_ratings(feature_df, num_resorts_to_rate):
     resorts = list(feature_df.index)
     # empty dictionary to store user ratings
     user_ratings = {}
-    # simulate random ratings for each resort 
-    for resort in resorts:
+    # randomly select resorts to rate
+    resorts_to_rate = random.sample(resorts, num_resorts_to_rate)
+    # simulate random ratings for selected resorts
+    for resort in resorts_to_rate:
         user_ratings[resort] = random.randint(1, 5)
     return user_ratings
 
-def predict_user_rating(feature_df, user_ratings):
-    # get list of resorts from dataframe index
-    resorts = list(feature_df.index)
-    # get unrated resorts
-    unrated_resorts = [resort for resort in resorts if resort not in user_ratings]
-    # pick a random unrated resort to predict rating of
-    target = random.choice(unrated_resorts)
+def predict_user_rating(feature_df, user_ratings, target_resort):
     # compute the similarity between other resorts
     similarity_df = pd.DataFrame(
         cosine_similarity(feature_df),
         index=feature_df.index,
         columns=feature_df.index
     )
-    # get the similarities between the target resort and the resorts that the user has rated
     # turn user_ratings into series for easier manipulation
     user_ratings = pd.Series(user_ratings)
 
     # get the similarities between the target resort and the resorts that the user has rated
-    user_rated_resorts_sims = similarity_df.loc[target, user_ratings.index]
+    user_rated_resorts_sims = similarity_df.loc[target_resort, user_ratings.index]
     # get user ratings for those resorts
     ratings = user_ratings[user_rated_resorts_sims.index]
     # get weighted average with ratings and cosine similarity scores
@@ -50,6 +47,10 @@ def predict_user_rating(feature_df, user_ratings):
     term2 = np.sum(np.abs(user_rated_resorts_sims))
     # produce rating
     prediction = term1 / term2
-    print(f"\nPredicted user rating for '{target}': {prediction:.4f}")
+    return prediction
+
+
+
+
 
 
